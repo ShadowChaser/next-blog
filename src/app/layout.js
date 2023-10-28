62549c247dd31ecae86fb1bd20bf87abe7f878b6
Abhishek Kundu <abhishekkund86@gmail.com>
2023-10-28 21:48:38 +0530

    Basic Structure and toogle Button

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from "@/component/navbar/Navbar";
 import Footer from "@/component/footer/Footer";
+import { ThemeContextProvider } from '@/context/ThemeContext';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeContextProvider>
         <div className='container'>
           <div className='wrapper'>
           <Navbar/>
@@ -22,6 +24,7 @@ export default function RootLayout({ children }) {
           <Footer/>
           </div>
         </div>  
+        </ThemeContextProvider>
         </body>
     </html>
   )
